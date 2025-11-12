c061592b1be2158fd64c155275fa80cd54fcc6ed
eltnpay <elyorkarimbaev55@gmail.com>
2025-11-12 15:27:02 +0500

    написал код на models.py и все успешно

--- app/models.py
+++ app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        unique=True, 
+        verbose_name='Название категории'
+        )
+    description = models.TextField(
+        blank=True, 
+        null=True, 
+        verbose_name='Описание'
+        )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        unique=True, 
+        verbose_name='Название бренда'
+        )
+    country = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='Страна производителя'
+        )
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=200, 
+        verbose_name='Название товара'
+        )
+    description = models.TextField(
+        verbose_name='Описание'
+        )
+    price = models.PositiveBigIntegerField(
+        verbose_name='Цена'
+        )
+    image = models.ImageField(
+        upload_to='products/', 
+        verbose_name='Изображение товара')
+    in_stock = models.PositiveIntegerField(
+        default=0, 
+        verbose_name='Количество на складе')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name="Категория")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products', verbose_name="Бренд")
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.title
--- eg_shop/settings.py
+++ eg_shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -102,19 +103,33 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
+#Команда для сборки
+# python manage.py collectstatic
+# Она берёт все файлы из всех static/ папок в проекте (в приложениях и глобальной) и копирует их в папку, указанную в STATIC_ROOT.
+# После этого веб-сервер может отдавать эти файлы напрямую, без участия Django.
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'       
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles"    
+
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
