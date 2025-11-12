c1b709846aed94aa64405fd94afe0c0776cb9ee1
eltnpay <elyorkarimbaev55@gmail.com>
2025-11-12 16:02:37 +0500

    добавил пути в admin.py и добавил продукты

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import Category,Brand,Product
 
+
+admin.site.register
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
