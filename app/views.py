761072c781ca05ce1933ab67a964095b4b8cc0be
eltnpay <elyorkarimbaev55@gmail.com>
2025-11-13 14:57:54 +0500

    добавил там templates и хтмл файлы

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.shortcuts import render,get_object_or_404
+from django.db.models import Q
 # Create your views here.
+
+
+
+def product_list(request):
+    products = Product.objects.all()  # достаём все записи из таблицы Product
+    return render(request, 'product_list.html', {'products': products})
